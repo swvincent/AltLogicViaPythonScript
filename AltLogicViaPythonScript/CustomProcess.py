fbf378b4598a3811deff2f0c6f6c073c4004c9e0
Scott W. Vincent <scott@swvincent.com>
2025-03-25 21:44:43 -0400

    Custom logic apply 5% discount

--- AltLogicViaPythonScript/CustomProcess.py
+++ AltLogicViaPythonScript/CustomProcess.py
@@ -9,12 +9,23 @@ col = db["PurchaseOrders"]
 
 myFilter = { '_id' : ObjectId('67e35154716e79e09821e062')}
 
+# Set AdjustedOrderTotal to OrderTotal, then apply 5% discount
 pipeline = [
    {
-      "$set": {
-         "AdjustedOrderTotal": "$OrderTotal"
-      }
+       "$set": {
+           "AdjustedOrderTotal": "$OrderTotal"
+       }
+   },
+   {
+       "$set": {
+           "AdjustedOrderTotal": {
+               "$multiply": [ 0.95, "$AdjustedOrderTotal" ]
+           }
+       }
    },
 ]
 
 results = col.update_one(myFilter, pipeline)
+
+# TODO: Return this?
+print(results.acknowledged)
